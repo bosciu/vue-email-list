b2d6f06313c847c899fe385827d5da7d6d56003b
bosciu <bosciu@icloud.com>
2021-05-27 14:45:27 +0200

    Stampa delle email in pagina

--- js/script.js
+++ js/script.js
@@ -4,13 +4,18 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista. */
 
 const app = new Vue({
     el: "#root",
-    data: {},
+    data: {
+        emails: []
+    },
     methods: {},
     mounted: function() {
-        axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then(function(element) {
-                console.log(element);
-            });
+        for (let i = 0; i < 10; i++) {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((element) => {
+                    const email = element.data.response;
+                    this.$data.emails.push(email);
+                });
+        }
     }
 });
